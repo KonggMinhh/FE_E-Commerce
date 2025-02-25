c8ce1becab0193d128d109bf310766e0a94563c7
KonggMinhh <hoangkongminh2001@gmail.com>
2025-02-25 23:57:43 +0700

    Remove unnecessary whitespace in CardProduct component

--- src/components/CardProduct.js
+++ src/components/CardProduct.js
@@ -20,9 +20,7 @@ const CardProduct = ({ productData }) => {
                     {productData?.price}
                 </span>
                 {/* Rating start */}
-                
 
-                
                 {/* Button Add */}
             </div>
         </article>
