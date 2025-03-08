427e18419462d1434337a1f0b3cf79df466c79e0
KonggMinhh <hoangkongminh2001@gmail.com>
2025-03-08 12:00:00 +0700

    Commit vào ngày 8 tháng 3 năm 2025

--- src/components/CardProduct.js
+++ src/components/CardProduct.js
@@ -21,7 +21,6 @@ const CardProduct = ({ productData }) => {
                 </span>
                 {/* Rating start */}
 
-
                 
                 {/* Button Add */}
             </div>
